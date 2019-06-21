ca760679a2aa8260b7efe3864d41ddf1b86cd11a
sbx_user1051 <sbx_user1051@169.254.67.61>
2019-06-21 10:47:45 +0000

    Fri Jun 21 10:47:45 UTC 2019

--- src/main/java/befaster/solutions/HLO/HelloSolution2.java
+++ src/main/java/befaster/solutions/HLO/HelloSolution2.java
@@ -1,19 +1,20 @@
 package befaster.solutions.HLO;
 
 public class HelloSolution2 {
-	
-	//String hello(string name)---> Message with name 
-	//two ways one is concatenate the strings and other user String builder
-	
+
+	// String hello(string name)---> Message with name
+	// two ways one is concatenate the strings and other user String builder
+
 	public String hello(String name) {
 		StringBuilder str = new StringBuilder();
 		String greetingMessage = "Hello";
 		StringBuilder messageInString = str.append(greetingMessage).append(",").append(" ").append(name).append("!");
-		
-		return null;
-		
+		String actualMessageInString = messageInString.toString();
+
+		return actualMessageInString;
+
 	}
-	
 
 }
+
 
